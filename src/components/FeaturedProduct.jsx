1aa59d3e927ebe353fb670366ed1c90ee07ec5cf
Imtiaz Ahmad Tanvir <imtiaztanveer07@gmail.com>
2025-02-19 11:55:37 +0600

    change the footer

--- src/components/FeaturedProduct.jsx
+++ src/components/FeaturedProduct.jsx
@@ -25,7 +25,7 @@ const FeaturedProducts = () => {
   }, []);
 
   return (
-    <section className="py-12 bg-gray-100">
+    <section className="py-12">
       <div className="container mx-auto">
         <h2 className="text-3xl text-gray-600 font-bold text-center mb-8">Featured Products</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
